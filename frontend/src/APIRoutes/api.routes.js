cc78c2552bc72e86ee4e328352db4258da2d33a0
ritesh0304 <ranaritesh0304@gmail.com>
2024-06-23 22:33:23 +0530

    deployment debugging78

--- frontend/src/APIRoutes/api.routes.js
+++ frontend/src/APIRoutes/api.routes.js
@@ -1,5 +1,5 @@
 // export const  hostname=" http://localhost:3000/api/v1"
-export const  hostname="https://todo-management-beta.vercel.app"
+export const  hostname="https://todo-management-tdr2.onrender.com"
 
 export const authRegister=`${hostname}/auth/register`;
 export const authLogin=`${hostname}/auth/login`
